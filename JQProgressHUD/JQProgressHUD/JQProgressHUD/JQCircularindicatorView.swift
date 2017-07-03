7176ea5eb96c80c052f730bcb7ba585bd6cc6806
huangjiaqian <18775134221@163.com>
2017-07-03 14:47:38 +0800

    增加新功能

---
+++ JQProgressHUD/JQProgressHUD/JQProgressHUD/JQCircularindicatorView.swift
@@ -0,0 +1,140 @@
+//
+//  JQCircularindicatorView.swift
+//  JQProgressHUD
+//
+//  Created by HJQ on 2017/7/3.
+//  Copyright © 2017年 HJQ. All rights reserved.
+//
+
+import UIKit
+
+public class JQCircularindicatorView: UIView {
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configParams()
+        setupUI()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public var progress: CGFloat = 0 {
+        didSet{
+            setProgress(progress: progress)
+        }
+    }
+    
+    public var textSize: CGFloat = 15 {
+        didSet{
+            progressLabel.font = UIFont.systemFont(ofSize: textSize)
+        }
+    }
+    
+    public var annularColor: UIColor = UIColor.white  {
+        didSet{
+            progressLabel.textColor = annularColor
+            annularLayer.strokeColor = annularColor.cgColor
+        }
+    }
+    
+    public var labelVisible = true {
+        didSet{
+            if !labelVisible { progressLabel.removeFromSuperview() }
+        }
+    }
+    
+
+    // MARK: - private methods
+    fileprivate func configParams() {
+        
+        progressLabel.text = "0%"
+        lineWidth = frame.size.height < frame.size.width ? frame.size.height/20 : frame.size.width/20
+        radius = frame.size.height < frame.size.width ? frame.size.height/2 - lineWidth/2 : frame.size.width/2 - lineWidth/2
+        
+        backgroundLayer.fillColor = UIColor.clear.cgColor
+        backgroundLayer.strokeColor = UIColor.gray.cgColor
+        backgroundLayer.lineWidth = lineWidth
+        backgroundLayer.lineCap = kCALineCapRound
+        
+        annularLayer.fillColor = UIColor.clear.cgColor
+        annularLayer.strokeColor = annularColor.cgColor
+        annularLayer.lineWidth = lineWidth
+        annularLayer.lineCap = kCALineCapRound
+    }
+    
+    fileprivate func setupUI(){
+        
+        // 绘制 path
+        let startAngle: CGFloat = 0
+        let endAngle = CGFloat.pi * 2.0
+        let arcCenter = CGPoint(x: frame.size.width/2,
+                                y: frame.size.height/2)
+        let path = UIBezierPath(arcCenter: arcCenter,
+                                radius: radius ,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        backgroundLayer.path = path.cgPath
+        layer.addSublayer(backgroundLayer)
+        
+        addSubview(progressLabel)
+    }
+    
+    override public func layoutSubviews() {
+        
+        // 设置 progressLabel 的frame
+        let pw =  (radius - lineWidth/2) * (radius - lineWidth/2) * 2
+        progressLabel.frame.size = CGSize(width: sqrt(pw), height: sqrt(pw))
+        progressLabel.center = CGPoint(x: frame.size.width/2,
+                                       y: frame.size.height/2)
+    }
+    
+    
+    fileprivate func setProgress(progress: CGFloat) {
+        
+        if 0 <= progress && progress <= 100.0 {
+            
+            let startAngle: CGFloat = -(CGFloat.pi / 2.0)
+            let endAngle = CGFloat.pi * 1.5
+            let arcCenter = CGPoint(x: frame.size.width/2,
+                                    y: frame.size.height/2)
+            
+            let path = UIBezierPath(arcCenter: arcCenter,
+                                    radius: radius ,
+                                    startAngle: startAngle,
+                                    endAngle: endAngle,
+                                    clockwise: true)
+            
+            annularLayer.path = path.cgPath
+            layer.addSublayer(annularLayer)
+            annularLayer.strokeEnd = progress / 100
+            progressLabel.text = "\(progress)%"
+        }
+    }
+    
+    // MARK: - lazy load
+    fileprivate var progressLabel: UILabel = {
+        let progressLabel = UILabel()
+        progressLabel.textAlignment = NSTextAlignment.center
+        return progressLabel
+    }()
+    
+    fileprivate var lineWidth: CGFloat!
+    
+    fileprivate var radius: CGFloat!
+    
+    fileprivate lazy var backgroundLayer: CAShapeLayer = {
+        let backgroundLayer = CAShapeLayer()
+        return backgroundLayer
+    }()
+    
+    fileprivate lazy var annularLayer: CAShapeLayer = {
+        let annularLayer = CAShapeLayer()
+        return annularLayer
+    }()
+
+
+}
--- JQProgressHUD/JQProgressHUD/ViewController.swift
+++ JQProgressHUD/JQProgressHUD/ViewController.swift
@@ -10,31 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
+        
     }
     
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //JQProgressHUDTool.jq_showHUD()
-        //JQProgressHUDTool.jq_showToast()
+    // MARK: - private methods
+    private func setupUI() {
+        self.tableView.tableFooterView = UIView.init()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func showHUDAction(_ sender: Any) {
-        JQProgressHUDTool.jq_showHUD(view: self.view, msg: "加载中")
-    }
-    @IBAction func showToastAction(_ sender: Any) {
-        JQProgressHUDTool.jq_showToast(view: self.view, msg: "测试")
-    }
-    @IBAction func hideHudAction(_ sender: Any) {
-        JQProgressHUDTool.jq_hideHUD(view: self.view, animation: true)
-    }
+    // MARK: -  lazy load
+    fileprivate var datas: [String] = ["normalHUD","customHUD","progressHUD","showImageHUD","toastHUD","hideHUD"]
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: String.init(describing: UITableViewCell.self))
+        if cell == nil {
+            cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: String.init(describing: UITableViewCell.self))
+        }
+        cell?.textLabel?.text = datas[indexPath.row]
+        return cell!
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch indexPath.row {
+        case 0:
+            JQProgressHUDTool.jq_showNormalHUD(msg: "加载中...")
+            break
+        case 1:
+            JQProgressHUDTool.jq_showCustomHUD(msg: "加载中...")
+            break
+        case 2:
+            DispatchQueue.global().async {
+                
+                for i in 1...100 {
+                    Thread.sleep(forTimeInterval: 0.02)
+                    DispatchQueue.main.async {
+                        JQProgressHUDTool.jq_showCircularHUD(msg: "加载中...", progress: CGFloat(i))
+                    }
+                }
+                Thread.sleep(forTimeInterval: 0.1)
+                DispatchQueue.main.async {
+                    
+                }
+            }
+            break
+        case 3:
+            JQProgressHUDTool.jq_showImageHUD( msg: "错误", imageName: "Error")
+            break
+        case 4:
+            JQProgressHUDTool.jq_showToastHUD( msg: "我是一个toast")
+            break
+        case 5:
+            JQProgressHUDTool.jq_hideHUD()
+            break
+            
+        default: break
+            
+        }
+    }
 }
 
