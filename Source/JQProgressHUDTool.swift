8b7fb43dc98a77241846df4ec04827860305ce66
huangjiaqian <18775134221@163.com>
2017-07-04 22:09:30 +0800

    方便使用工具类增加参数

--- Source/JQProgressHUDTool.swift
+++ Source/JQProgressHUDTool.swift
@@ -11,47 +11,54 @@ import UIKit
 
 public class JQProgressHUDTool: NSObject {
 
-    public class func jq_showNormalHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "", needMask: Bool? = false) {
+    @discardableResult
+    public class func jq_showNormalHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",  isNeedmask: Bool? = false, isUserInteractionEnabled: Bool? = false, animation: Bool? = false) -> JQProgressHUD {
         
-        let hud: JQProgressHUD = JQProgressHUD.showHUD(addTo: view!)
-        hud.isNeedMask = needMask!
+        let hud: JQProgressHUD = JQProgressHUD.showHUD(addTo: view!, animation: animation)
+        hud.isNeedMask = isNeedmask!
         hud.detailLabel.text = msg!
         let activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView.init(activityIndicatorStyle: UIActivityIndicatorViewStyle.white)
         activityIndicatorView.frame = CGRect.init(x: 0, y: 0, width: 30, height: 30)
         activityIndicatorView.startAnimating()
         hud.indicatorView = activityIndicatorView
-        hud.isUserInteractionEnabled = needMask!
+        hud.isUserInteractionEnabled = isUserInteractionEnabled!
+        return hud
     }
     
-    public class func jq_showCustomHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "", needMask: Bool? = false){
+    @discardableResult
+    public class func jq_showCustomHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "", isNeedmask: Bool? = false, isUserInteractionEnabled: Bool? = false, animation: Bool? = false) -> JQProgressHUD {
         
-        let hud: JQProgressHUD = JQProgressHUD.showHUD(addTo: view!)
-        hud.isNeedMask = needMask!
+        let hud: JQProgressHUD = JQProgressHUD.showHUD(addTo: view!, animation: animation!)
+        hud.isNeedMask = isNeedmask!
         hud.detailLabel.text = msg!
         let indicatorView: JQIndicatorView = JQIndicatorView.init(frame: CGRect.init(x: 0, y: 0, width: 30, height: 30))
         indicatorView.color = UIColor.white
         hud.indicatorView = indicatorView
-        hud.isUserInteractionEnabled = needMask!
+        hud.isUserInteractionEnabled = isUserInteractionEnabled!
+        return hud
     }
     
-    public class func jq_showCircularHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",progress: CGFloat? = 0, needMask: Bool? = false) {
+    @discardableResult
+    public class func jq_showCircularHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",progress: CGFloat? = 0, isNeedmask: Bool? = false, isUserInteractionEnabled: Bool? = false, animation: Bool? = false) -> JQProgressHUD {
         
         var  hud: JQProgressHUD
-        hud = JQProgressHUD.showHUD(addTo: view!)
-        hud.isNeedMask = needMask!
+        hud = JQProgressHUD.showHUD(addTo: view!, animation: animation!)
+        hud.isNeedMask = isNeedmask!
         hud.detailLabel.text = msg!
         let circularindicator = JQCircularindicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         circularindicator.annularColor = UIColor.white
         circularindicator.textSize = 8
         circularindicator.progress = progress!
         hud.indicatorView = circularindicator
-        hud.isUserInteractionEnabled = needMask!
+        hud.isUserInteractionEnabled = isUserInteractionEnabled!
+        return hud
     }
     
-    public class func jq_showImageHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",imageName: String? = "", needMask: Bool? = false) {
+    @discardableResult
+    public class func jq_showImageHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",imageName: String? = "", isNeedmask: Bool? = false, isUserInteractionEnabled: Bool? = false, animation: Bool? = false) -> JQProgressHUD {
         
         let hud: JQProgressHUD = JQProgressHUD.showHUD(addTo: view!)
-        hud.isNeedMask = needMask!
+        hud.isNeedMask = isNeedmask!
         hud.detailLabel.text = msg!
         hud.duration = 3.0
         let imageView: UIImageView = UIImageView.init()
@@ -60,15 +67,18 @@ public class JQProgressHUDTool: NSObject {
         imageView.image = UIImage.init(named: imageName ?? "")
         hud.indicatorView = imageView
         hud.containerViewBGcolor = UIColor.orange
-        hud.isUserInteractionEnabled = needMask!
+        hud.isUserInteractionEnabled = isUserInteractionEnabled!
+        return hud
     }
     
-    public class func jq_showToastHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",duration: TimeInterval? = 3.0) {
+    @discardableResult
+    public class func jq_showToastHUD(view: UIView? = UIApplication.shared.windows.last, msg: String? = "",duration: TimeInterval? = 3.0, isUserInteractionEnabled: Bool? = false, animation: Bool? = false) -> JQProgressHUD {
         
-        let hud: JQProgressHUD = JQProgressHUD.showToastHUD(addTo: view!)
+        let hud: JQProgressHUD = JQProgressHUD.showToastHUD(addTo: view!, animation: animation!)
         hud.duration = duration!
         hud.toastLabel.text = msg
-        hud.isUserInteractionEnabled = false
+        hud.isUserInteractionEnabled = isUserInteractionEnabled!
+        return hud
     }
     
     public class func jq_hideHUD(view: UIView? = UIApplication.shared.windows.last) {
